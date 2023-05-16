b030a12cf91b8ee52ea034f1442d1f4182ee1319
Joe Fitzgerald <joesph.fitzgerald@verizon.com>
2023-05-16 14:08:22 -0400

    Add project files.

---
+++ EmpowerJGF_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerJGF_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerJGF_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("839DDF24-1E26-46A3-B9E8-7C0F60B4D780")]
+[assembly: AssemblyVersion("1.0.0.0")]
